354e01cfaf57be454a65f595cfb42659e9848ba0
Reuben Donnison <reujdon@gmail.com>
2022-10-15 12:39:00 +1300

    Fixed node

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-import express from "express";
-import path from "path";
-import cors from "cors";
-import("dotenv");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 //*Routes
-import projects from "./routes/projects.js";
+const projects = require("./routes/projects.js");
 app.use("/projects", projects);
 
 app.get("/", (req, res) => {
--- routes/projects.js
+++ routes/projects.js
@@ -1,7 +1,7 @@
-import express from "express";
+const express = require("express");
 const router = express.Router();
 
-import axios from "axios";
+const axios = require("axios");
 
 router.get("/", function (_req, res) {
   res.render("hub");
