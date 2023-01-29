b7cbf18e91dd305f5a3eccecf3356cfbcd2989a1
Reuben Donnison <reujdon@gmail.com>
2023-01-29 13:02:16 +1300

    Added border and scroll icon. Also added letter random effect

---
+++ index.js
@@ -0,0 +1,28 @@
+const letters = "abcdefghijklmnopqrstuvwxyz";
+
+let interval = null;
+
+target = document.querySelector("h1");
+
+let iteration = -2;
+
+clearInterval(interval);
+
+interval = setInterval(() => {
+  target.innerText = target.innerText
+    .split("")
+    .map((letter, index) => {
+      if (index < iteration) {
+        return target.dataset.value[index];
+      }
+
+      return letters[Math.floor(Math.random() * 26)];
+    })
+    .join("");
+
+  if (iteration >= target.dataset.value.length) {
+    clearInterval(interval);
+  }
+
+  iteration += 1 / 4;
+}, 30);
