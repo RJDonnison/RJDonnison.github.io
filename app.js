026e73686a8d04cfda6f03a61831157b63099fda
Reuben Donnison <reujdon@gmail.com>
2023-08-31 19:04:45 +1200

    Added github contention

--- app.js
+++ app.js
@@ -12,6 +12,41 @@ const port =
     ? process.env.PROD_PORT
     : process.env.DEV_PORT;
 
+//Import cors
+const cors = require("cors");
+app.use(cors());
+
+//Import bodyParser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Import axios
+const axios = require("axios");
+
+//Get github data
+//!Not returning data
+const githubData = async () => {
+  try {
+    const response = await axios({
+      method: "get",
+      url: `https://api.github.com/users/${process.env.GITHUB_USERNAME}/repos`,
+      headers: {
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        "Content-Type": "application/json",
+        Accept: "application/vnd.github.mercy-preview+json", // MUST ADD TO INCLUDE TOPICS
+      },
+    });
+
+    if (response.status === 200) {
+      return response;
+    }
+    return false;
+  } catch (err) {
+    return err;
+  }
+};
+
 //Set static file location
 app.use("/static", express.static(path.join(__dirname, "public")));
 
@@ -21,6 +56,8 @@ app.set("view engine", "ejs");
 
 //Display page at /
 app.get("/", (req, res) => {
+  console.log(githubData);
+
   res.render("index");
 });
 
