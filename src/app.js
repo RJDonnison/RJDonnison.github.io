d82792c9ca2dc3db0b4a44abcf63538c3c188d1a
Reuben Donnison <reujdon@gmail.com>
2023-08-31 17:57:09 +1200

    Express app setup

--- src/app.js
+++ src/app.js
@@ -1,93 +1,26 @@
-//*Random letters on load
-//#region
-//Selectable letters
-const letters = "abcdefghijklmnopqrstuvwxyz";
+//Import path
+const path = require("path");
 
-let interval = null;
+//Import express
+const express = require("express");
+const app = express();
+const port = 3000;
 
-//Get target element
-let target = document.querySelector("h1");
+//Set static file location
+app.use("/static", express.static(path.join(__dirname, "public")));
 
-//Set -ve interval to randomize first letters of text
-let iteration = -2;
+//Set view file location and type
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-clearInterval(interval);
-
-interval = setInterval(() => {
-  target.innerText = target.innerText
-    .split("")
-    .map((letter, index) => {
-      //If iteration is greater than letter index stop randomizing that letter
-      if (index < iteration) {
-        return target.dataset.value[index];
-      }
-
-      //Return random letter
-      return letters[Math.floor(Math.random() * 26)];
-    })
-    .join("");
-
-  //If iterations greater than length of default value end loop
-  if (iteration >= target.dataset.value.length) {
-    clearInterval(interval);
-  }
-
-  //Increase iteration. Fraction is number of times letters will randomize
-  iteration += 1 / 4;
-}, 30);
-//#endregion
-
-//*gsap animations
-//#region
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
-
-//Get timeline
-let timeline = document.getElementById("timeline");
-
-//Set width of projects by adding element sizes
-let projects = document.getElementById("projects");
-let width = -window.screen.width;
-for (const element of projects.children) width += element.offsetWidth;
-
-//Timeline horizontal scroll
-gsap.to("#projects", {
-  x: `-${width}px`,
-  scrollTrigger: {
-    trigger: timeline,
-    start: `start end`,
-    end: () => `+=${timeline.offsetWidth}`,
-    pin: true,
-    pinSpacing: false,
-    scrub: true,
-  },
-  ease: "none",
+//Display page at /
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-//Timeline scroll progress bar
-gsap.to("#timeline progress", {
-  value: 100,
-  scrollTrigger: {
-    trigger: timeline,
-    start: `start end`,
-    end: () => `+=${timeline.offsetWidth}`,
-    scrub: true,
-  },
-  ease: "none",
+//Make app live at port
+app.listen(port, () => {
+  console.log(
+    `Portfolio app listening on port ${port} at http://localhost:${port}/`
+  );
 });
-
-//Keep footer at bottom of timeline
-gsap.to("footer", {
-  y: timeline.offsetWidth,
-  scrollTrigger: {
-    trigger: timeline,
-    start: `start end`,
-    end: () => `+=${timeline.offsetWidth}`,
-    pinSpacing: false,
-    scrub: true,
-  },
-  ease: "none",
-});
-//#endregion
